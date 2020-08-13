5ef5eecd685970a056b78c12b3be6681b4b04218
Niels Daalhuisen <niels.daalhuisen@kubion.nl>
2020-08-13 17:17:05 +0200

    java version 11

--- src/test/java/nl/nielsdaalhuisen/lingowords/LingowordsApplicationTests.java
+++ src/test/java/nl/nielsdaalhuisen/lingowords/LingowordsApplicationTests.java
@@ -1,13 +1,29 @@
 package nl.nielsdaalhuisen.lingowords;
 
+import nl.nielsdaalhuisen.lingowords.domain.Word;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class LingowordsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Validate false word")
+	void validateFalseWord() {
+		Word word = new Word("10-tal");
+		assertFalse(word.isValid());
+	}
+
+	@Test
+	@DisplayName("Validate valid word")
+	void validateValidWord() {
+		Word word = new Word("mislukt");
+		assertTrue(word.isValid());
 	}
 
 }
